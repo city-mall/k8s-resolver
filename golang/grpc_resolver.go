ea38c7fcdf5aa70326d67d99e15130dc881224c4
Arkadev Banerjee <arkadev.banerjee@citymall.live>
2023-07-19 11:51:51 +0530

    Golang

---
+++ golang/grpc_resolver.go
@@ -0,0 +1,87 @@
+package client
+
+import (
+	"context"
+	"strings"
+	"sync"
+
+	r "google.golang.org/grpc/resolver"
+)
+
+const K8sScheme = "k8s"
+
+// Short Note on UpdateState
+// UpdateState updates the state of the ClientConn appropriately.
+//
+// If an error is returned, the resolver should try to resolve the
+// target again. The resolver should use a backoff timer to prevent
+// overloading the server with requests. If a resolver is certain that
+// reresolving will not change the result, e.g. because it is
+// a watch-based resolver, returned errors can be ignored.
+//
+// If the resolved State is the same as the last reported one, calling
+// UpdateState can be omitted.
+
+type K8sResolverBuilder struct {
+}
+
+type k8sEventDrivenResolver struct {
+	resolver *internalResolver
+	wg       *sync.WaitGroup
+}
+
+// ResolveNow will be called by gRPC to try to resolve the target name
+// again. It's just a hint, resolver can ignore this if it's not necessary.
+//
+// It could be called multiple times concurrently.
+func (k *k8sEventDrivenResolver) ResolveNow(opts r.ResolveNowOptions) {
+
+}
+
+// Close closes the resolver.
+func (k *k8sEventDrivenResolver) Close() {
+	k.resolver.close()
+	k.wg.Wait()
+}
+
+// Build creates a new resolver for the given target.
+//
+// gRPC dial calls Build synchronously, and fails if the returned error is
+// not nil.
+func (b *K8sResolverBuilder) Build(target r.Target, cc r.ClientConn, opts r.BuildOptions) (r.Resolver, error) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	namespace := target.URL.Hostname()
+	service := target.Endpoint()
+	idx := strings.Index(service, ":")
+	serviceName := service[:idx]
+	port := service[idx+1:]
+	notifier := make(chan struct{})
+	resolver := newInternalResolver(context.Background(), serviceName, namespace, notifier)
+	go func() {
+		for range notifier {
+			ips := resolver.getIPs()
+			addresses := make([]r.Address, 0, len(ips))
+			for ip := range ips {
+				addresses = append(addresses, r.Address{Addr: ip + ":" + port})
+			}
+			if err := cc.UpdateState(r.State{Addresses: addresses}); err != nil {
+				panic(err)
+			}
+		}
+	}()
+	go resolver.start(&wg)
+
+	return &k8sEventDrivenResolver{
+		wg:       &wg,
+		resolver: resolver,
+	}, nil
+}
+
+// Scheme returns the scheme supported by this r.Scheme is defined
+// at https://github.com/grpc/grpc/blob/master/doc/naming.md.  The returned
+// string should not contain uppercase characters, as they will not match
+// the parsed target's scheme as defined in RFC 3986.
+func (b *K8sResolverBuilder) Scheme() string {
+	return K8sScheme
+}
---
+++ golang/internal_resolver.go
@@ -0,0 +1,141 @@
+package client
+
+import (
+	"context"
+	"sync"
+
+	"github.com/rs/zerolog/log"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+)
+
+type internalResolver struct {
+	mu sync.RWMutex
+
+	ctx       context.Context
+	informer  cache.SharedIndexInformer
+	addresses map[string]bool
+	stop      chan struct{}
+}
+
+func newInternalResolver(ctx context.Context, service string, namespace string, notify chan struct{}) *internalResolver {
+	var r internalResolver
+	r.addresses = make(map[string]bool)
+	r.ctx = ctx
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	informer := cache.NewSharedIndexInformer(
+		&cache.ListWatch{
+			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+				options.FieldSelector = "metadata.name=" + service
+				return clientset.CoreV1().Endpoints(namespace).List(ctx, options)
+			},
+			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+				options.FieldSelector = "metadata.name=" + service
+				return clientset.CoreV1().Endpoints(namespace).Watch(ctx, options)
+			},
+		},
+		&v1.Endpoints{},
+		0,
+		cache.Indexers{},
+	)
+
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			endpoints := obj.(*v1.Endpoints)
+			ok := r.handleUpsert(endpoints.Subsets)
+			log.Debug().Str("Component", "Resolver").Msgf("Endpoints added: %s", endpoints.Name)
+			if ok && notify != nil {
+				notify <- struct{}{}
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			endpoints := obj.(*v1.Endpoints)
+			ok := r.handleDelete(endpoints.Subsets)
+			log.Debug().Str("Component", "Resolver").Msgf("Endpoints deleted: %s", endpoints.Name)
+			if ok && notify != nil {
+				notify <- struct{}{}
+			}
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			oldEndpoints := oldObj.(*v1.Endpoints)
+			newEndpoints := newObj.(*v1.Endpoints)
+			if oldEndpoints.ResourceVersion == newEndpoints.ResourceVersion {
+				return
+			}
+			ok := r.handleUpsert(newEndpoints.Subsets)
+			log.Debug().Str("Component", "Resolver").Msgf("Endpoints updated: %s", newEndpoints.Name)
+			if ok && notify != nil {
+				notify <- struct{}{}
+			}
+		},
+	})
+
+	r.informer = informer
+
+	return &r
+}
+
+func (r *internalResolver) start(wg *sync.WaitGroup) {
+	defer wg.Done()
+	stop := make(chan struct{})
+	r.stop = stop
+	go r.informer.Run(stop)
+
+	<-r.ctx.Done()
+	close(stop)
+}
+
+func (r *internalResolver) close() {
+	close(r.stop)
+}
+
+func (r *internalResolver) handleUpsert(subsets []v1.EndpointSubset) bool {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	addresses := make(map[string]bool)
+	for _, sub := range subsets {
+		for _, point := range sub.Addresses {
+			addresses[point.IP] = true
+		}
+	}
+
+	r.addresses = addresses
+	log.Debug().Str("Component", "Resolver.handleUpsert").Msgf("New address list: %v", addresses)
+	return true
+}
+
+func (r *internalResolver) handleDelete(subsets []v1.EndpointSubset) bool {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	for _, sub := range subsets {
+		for _, point := range sub.Addresses {
+			delete(r.addresses, point.IP)
+		}
+	}
+
+	log.Debug().Str("Component", "Resolver.handleDelete").Msgf("New address list: %v", r.addresses)
+	return true
+}
+
+func (r *internalResolver) getIPs() map[string]bool {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	return r.addresses
+}
