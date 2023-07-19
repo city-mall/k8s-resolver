e4046a883034f154fcc30893157ced4403258bc0
Arkadev Banerjee <arkadev.banerjee@citymall.live>
2023-07-19 11:59:31 +0530

    feat

--- golang/grpc_resolver.go
+++ golang/grpc_resolver.go
@@ -87,7 +87,8 @@ func (b *K8sResolverBuilder) Scheme() string {
 	return K8sScheme
 }
 
-func Try(address string) string {
+func Try(address string) (string, bool) {
+	var ok bool
 	if strings.Contains(address, "svc.cluster.local") {
 		hostServicePort := strings.Split(address, ":")
 		host := hostServicePort[0]
@@ -98,6 +99,7 @@ func Try(address string) string {
 		serviceNs := serviceNameServiceNs[1]
 
 		address = fmt.Sprintf("%s://%s/%s:%s", K8sScheme, serviceNs, serviceName, servicePort)
+		ok = true
 	}
-	return address
+	return address, ok
 }
